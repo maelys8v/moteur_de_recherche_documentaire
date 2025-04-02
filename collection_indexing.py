97a328873da5029a345f5e8a897ca99ebb5598d9
Lou Désert <lou.desert@insa-rennes.fr>
2025-04-02 15:08:50 +0200

    stopwords list

--- collection_indexing.py
+++ collection_indexing.py
@@ -1,13 +1,30 @@
 import spacy.cli
+# spacy.cli.download("en_core_web_sm") # Load English tokenizer because the texts
+nlp = spacy.load("en_core_web_sm")
 
 # ----------------------------------------------------
 import nltk
-nltk.download('punkt')
-nltk.download('punkt_tab')
+from nltk.corpus import stopwords # liste de stopwords de NLTK
+# nltk.download('punkt')
+# nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 texte = "ceci est une phrase. Je veux tester. Héhé"
 phrases = nltk.sent_tokenize(texte)
 print(phrases)
 # ------------------------------------------------------
 
-spacy.cli.download("en_core_web_md")
-nlp = spacy.load ( "en_core_web_md" )
+with open('CISI/DATA/CISI.ALLnettoye') as documents:
+    print(documents.read())
+
+# faire une boucle sur tous les fichiers
+# a chq fichier on tokenise
+
+
+# Choose what vocabulary we want to keep and do the associate processing
+stopWords = set(stopwords.words('english')) # Liste de stopwords de NLTK en anglais
+# Une liste de stopwords trouvés sur internet
+stopsWords_list = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+
+
