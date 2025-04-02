0873251f88b00bcff7d8e56766494f1e97a309dd
Maëlys Devaud <maelys.devaud@insa-rennes.fr>
2025-04-02 14:54:41 +0200

    nltk test

--- collection_indexing.py
+++ collection_indexing.py
@@ -1,3 +1,7 @@
 import spacy.cli
+import nltk
+texte = "ceci est une phrase. Je veux tester. Héhé"
+phrases = nltk.sent_tokenize(texte)
+print(phrases)
 spacy.cli.download("en_core_web_md")
 nlp = spacy.load ( "en_core_web_md" )
