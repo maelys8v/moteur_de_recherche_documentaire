dfee70ebcdc5464fc94d3230c6db21d72774ac9d
Léna Beust <lena.beust@insa-rennes.fr>
2025-04-02 14:43:58 +0200

    Modif Léna

--- collection_indexing.py
+++ collection_indexing.py
@@ -1,3 +1,9 @@
 import spacy.cli
-spacy.cli.download("en_core_web_md")
-nlp = spacy.load ( "en_core_web_md" )
+spacy.cli.download("en_core_web_sm") # Load English tokenizer because the texts
+nlp = spacy.load("en_core_web_sm")
+
+with open('CISI/DATA/CISI.ALLnettoye') as documents:
+    print(documents.read())
+
+# faire une boucle sur tous les fichiers
+# a chq fichier on tokenise
